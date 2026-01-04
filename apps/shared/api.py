d15dd195392e47f004d9b5ee97c1e76729e865a7
Mohammed Azzan Patni <azzaxp@gmail.com>
2026-01-04 10:33:52 +0530

    fix(api): Exempt FindWorkspaceView from CSRF to allow static page access

--- apps/shared/api.py
+++ apps/shared/api.py
@@ -234,6 +234,10 @@ class VerifyRegistrationOTPView(APIView):
         })
 
 
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
+
+@method_decorator(csrf_exempt, name='dispatch')
 class FindWorkspaceView(generics.GenericAPIView):
     """
     Find Masjid API with security protections:
